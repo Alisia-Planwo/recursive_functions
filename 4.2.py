8a9880dc4d1bcc201dc9f0134ffe227e594593f5
Alisia-Planwo <alisia.phini@planwo.com>
2024-11-28 17:29:10 +0530

    Add files via upload
    Flatten a nested dictionary:
    
    Input: {"a": {"b": {"c": 1}}, "d": 2}
    
    Output: {"a.b.c": 1, "d": 2}

---
+++ 4.2.py
@@ -0,0 +1,17 @@
+def flatten_dict(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
+nested_dict = {"a": {"b": {"c": 1}}, "d": 2}
+flattened = flatten_dict(nested_dict)
+
+print(flattened)
+
+
